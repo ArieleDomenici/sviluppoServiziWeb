61c1b0f11d7ca5710b9f807f2b9461dced4ff231
ArieleDomenici <arieledomenici@gmail.com>
2023-11-08 17:17:19 +0100

    dichiarato metodi archivio
    dichiarato i metodi vuoti per l'archivio

--- src/archive-access.service.ts
+++ src/archive-access.service.ts
@@ -9,5 +9,15 @@ export class ArchiveAccessService {
     new Book(3, 'Il Principe', 'Nicolò Machiavelli', ''),
   ]
   constructor() { }
+  //questo dovrà essere il metodo per andare a prendere l'array sul server
+  //dovrebbe restituire una stringa json credo
+  getarchive(): string{
+    return "dovrebbe essere la stringa json"
+  }
+  //questo dovrà essere il metodo per caricare il nuovo array coi cambiamenti
+  //non so cosa dovrebbe passargli penso di nuovo una stringa json?
+  postarchive(): void{
+
+  }
 
 }
