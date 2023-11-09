1f1119be0aff7c8b72efbdc98cf34be8c36d085a
ArieleDomenici <arieledomenici@gmail.com>
2023-11-09 15:25:47 +0100

    added component
    sono riuscito a far funzionare finalmente il template del component che non andava perchè non era standalone

---
+++ src/search-book/search-book.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-book',
+  standalone: true,
+  templateUrl: './search-book.component.html',
+  styleUrls: ['./search-book.component.css']
+})
+export class SearchBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
