7147811bdfa4e55c84f700cea8ac1561bb038a54
ArieleDomenici <arieledomenici@gmail.com>
2023-11-25 18:20:28 +0100

    aggiunto il form a add-book
    aggiunto il form a add-book e aggiunto il codice per fargli scrivere in console quello che viene scritto nel form

--- src/root/add-book/add-book.component.ts
+++ src/root/add-book/add-book.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
-  styleUrls: ['./add-book.component.css']
+  styleUrls: ['./add-book.component.css'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class AddBookComponent implements OnInit {
-
+  inputedBook = '';
+  searchBook(bookName : string){
+    console.log(bookName);
+  }
   constructor() { }
 
   ngOnInit() {
--- src/root/root.component.ts
+++ src/root/root.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, VERSION } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { SearchBookComponent } from './search-book/search-book.component';
+import { AddBookComponent } from './add-book/add-book.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.css'],
   standalone: true,
-  imports: [CommonModule, SearchBookComponent],
+  imports: [CommonModule, SearchBookComponent, AddBookComponent],
 })
 export class RootComponent implements OnInit {
   title: string = 'Angular ' + VERSION.major;
