7bfb83e3f967703eed15dad6288c28c23a995c94
ArieleDomenici <arieledomenici@gmail.com>
2023-12-03 18:50:25 +0100

    added observable test
    finalmente funziona l'observable.
    aggiunto un observable di prova che si attiva alla pressione del tasto in add-book

--- src/root/add-book/add-book.component.ts
+++ src/root/add-book/add-book.component.ts
@@ -1,27 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Observable, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
   styleUrls: ['./add-book.component.css'],
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-  ],
+  imports: [CommonModule, FormsModule],
 })
 export class AddBookComponent implements OnInit {
   inputedPosition = '';
   inputedTitle = '';
   inputedAuthor = '';
 
-  onSubmit(position: string, title : string, author : string){
+  observable = new Observable((subscriber) => {
+    subscriber.next(1);
+    subscriber.next(2);
+    subscriber.next(3);
+    setTimeout(() => {
+      subscriber.next(4);
+      subscriber.complete();
+    }, 1000);
+  });
+
+  onClick(){
+    this.observable.subscribe(value => console.log(value));
+  }
+
+  onSubmit(position: string, title: string, author: string) {
     console.log(position + title + author);
   }
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
